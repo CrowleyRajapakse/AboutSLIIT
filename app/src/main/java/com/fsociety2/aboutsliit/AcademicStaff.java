650f548c61e18d9d52bd8f0edabefc1e7bde7bad
Crowley Rajapakse <srrajapakse1@gmail.com>
2018-03-30 14:11:32 +0530

    adding setting intent in main page, share intents in chancellor page

--- app/src/main/java/com/fsociety2/aboutsliit/AcademicStaff.java
+++ app/src/main/java/com/fsociety2/aboutsliit/AcademicStaff.java
@@ -1,13 +1,70 @@
 package com.fsociety2.aboutsliit;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AcademicStaff extends AppCompatActivity {
 
+    Button btnFollowMe;
+    TextView txtMessenger;
+    TextView txtAddToGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_academic_staff);
+
+
+        btnFollowMe =  findViewById(R.id.follow_me);
+        txtMessenger = findViewById(R.id.messenger);
+        txtAddToGroup = findViewById(R.id.addtogroup);
+
+        btnFollowMe.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, "Hey, i need to follow you!");
+                startActivity(shareIntent);
+
+            }
+        });
+
+
+        txtMessenger.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, "Hey, i need to chat with you!");
+                startActivity(shareIntent);
+
+            }
+        });
+
+
+        txtAddToGroup.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, "Prof.Lalith Gamage");
+                startActivity(shareIntent);
+
+            }
+        });
+
     }
+
+
 }
