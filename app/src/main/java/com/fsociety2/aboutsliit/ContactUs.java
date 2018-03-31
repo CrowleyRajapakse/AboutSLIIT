472ebada7ae5c82c2a29e0d449f8044dc3955286
isuru jayathilaka <isuruj.94@gmail.com>
2018-04-01 05:18:00 +0530

    add toolbar in contact us activity

--- app/src/main/java/com/fsociety2/aboutsliit/ContactUs.java
+++ app/src/main/java/com/fsociety2/aboutsliit/ContactUs.java
@@ -2,8 +2,10 @@ package com.fsociety2.aboutsliit;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +21,11 @@ public class ContactUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Contact Us");
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
 
         final EditText your_name        = (EditText) findViewById(R.id.your_name);
         final EditText your_email       = (EditText) findViewById(R.id.your_email);
